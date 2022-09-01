f028a6fe1689af67872a26785b86b551ca6b9777
Danilo J. L. de Oliveira <danilojldeo@hotmail.com>
2022-09-01 19:20:12 -0300

    Starting library

---
+++ index.js
@@ -0,0 +1,6 @@
+
+import { NativeModules } from 'react-native';
+
+const { RNReactNativeSearchSelect } = NativeModules;
+
+export default RNReactNativeSearchSelect;
---
+++ ios/RNReactNativeSearchSelect.m
@@ -0,0 +1,13 @@
+
+#import "RNReactNativeSearchSelect.h"
+
+@implementation RNReactNativeSearchSelect
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+RCT_EXPORT_MODULE()
+
+@end
+  
