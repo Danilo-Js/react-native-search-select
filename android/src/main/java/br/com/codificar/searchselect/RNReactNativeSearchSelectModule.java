d80220db90d161fbf399a08ec37a2cb4ea26871a
Danilo J. L. de Oliveira <danilojldeo@hotmail.com>
2022-11-13 09:50:00 -0300

    Adding src/, Fixing Java Module

---
+++ android/src/main/java/br/com/codificar/searchselect/RNReactNativeSearchSelectModule.java
@@ -0,0 +1,22 @@
+
+package br.com.codificar.searchselect;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+
+public class RNReactNativeSearchSelectModule extends ReactContextBaseJavaModule {
+
+  private final ReactApplicationContext reactContext;
+
+  public RNReactNativeSearchSelectModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    this.reactContext = reactContext;
+  }
+
+  @Override
+  public String getName() {
+    return "RNReactNativeSearchSelect";
+  }
+}
